0d09dae9b74ad26a45828f7e599345a86c1dff22
Jordy <jordyquiroz14@gmail.com>
2018-07-31 10:04:13 -0400

    DOM

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor =" #d3c26f";
+
+let text = 
+document.querySelectorAll("h1")
+document.querySelectorAll("h1")[0].style.textShadow = "5px 5px 4px yellow";
+document.querySelectorAll("h1")[1].style.textShadow = "5px 5px 4px yellow";
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,9 @@
+
+function onClick(){
+    document.getElementById("rengar").src = "assets/rengar.png";
+    alert("You've been deleted");
+}
+
+const buttonElement = 
+document.addEventListener("click",onClick);
+
